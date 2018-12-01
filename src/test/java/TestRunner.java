4b5b0cb9b04e4cd80fc147516828da0edde3878d
Robert Dayton <robertcdayton@gmail.com>
2018-11-30 20:28:00 -0500

    Moved Test files to correct location

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,41 @@
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.CucumberFeatureWrapper;
+import cucumber.api.testng.PickleEventWrapper;
+import cucumber.api.testng.TestNGCucumberRunner;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+@CucumberOptions(
+        plugin = {"pretty","json:target/report/cucumber2.json"},
+        strict = true,
+        features = {"src/test/resources/features"},
+        glue = {"stepdefs"}
+)
+
+public class TestRunner {
+
+    private TestNGCucumberRunner testNGCucumberRunner;
+
+    @BeforeClass(alwaysRun = true)
+    public void setUpClass() throws Exception {
+        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+    }
+
+    @Test(groups = "Cucumber", description = "Runs Cucumber Feature", dataProvider = "scenarios")
+    public void scenario(PickleEventWrapper pickleEvent, CucumberFeatureWrapper cucumberFeature) throws Throwable {
+        testNGCucumberRunner.runScenario(pickleEvent.getPickleEvent());
+    }
+
+    @DataProvider
+    public Object[][] scenarios() {
+        return testNGCucumberRunner.provideScenarios();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDownClass() throws Exception {
+        testNGCucumberRunner.finish();
+    }
+
+}
---
+++ src/test/java/stepdefs/StepDefinitions.java
@@ -0,0 +1,100 @@
+package stepdefs;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class StepDefinitions {
+
+    @Given("The browser is open")
+    public void the_browser_is_open() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I can navigate to Yahoo Finance")
+    public void i_can_navigate_to_Yahoo_Finance() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("The URL is finance.yahoo.com")
+    public void the_URL_is_finance_yahoo_com() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I enter a stock code in the search field")
+    public void i_enter_a_stock_code_in_the_search_field() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I can be directed to the stock’s page")
+    public void i_can_be_directed_to_the_stock_s_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("I am on the given stock’s page")
+    public void i_am_on_the_given_stock_s_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I can retrieve the current price")
+    public void i_can_retrieve_the_current_price() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("I am on a given stock’s page")
+    public void i_am_on_a_given_stock_s_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I can retrieve the {int} week high and low")
+    public void i_can_retrieve_the_week_high_and_low(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("I have the current stock price and the {int} week high and low")
+    public void i_have_the_current_stock_price_and_the_week_high_and_low(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I can calculate the current price relative to the {int} week high and low")
+    public void i_can_calculate_the_current_price_relative_to_the_week_high_and_low(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("I have the current stock price and number of shares")
+    public void i_have_the_current_stock_price_and_number_of_shares() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I can calculate the earnings per share \\(EPS)")
+    public void i_can_calculate_the_earnings_per_share_EPS() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("I have the EPS of two stocks")
+    public void i_have_the_EPS_of_two_stocks() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I can determine which company has a higher EPS")
+    public void i_can_determine_which_company_has_a_higher_EPS() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+}
